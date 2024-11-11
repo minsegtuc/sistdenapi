4185aeb8f7359bc4a52ea0d5cb26ab8cf53b1f5d
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2024-11-11 08:11:55 -0300

    web socket

--- server/sockets/socketConfig.js
+++ server/sockets/socketConfig.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import Denuncia from "../models/denuncia.model.js";
 
 let wss;
 
@@ -6,14 +6,27 @@ export const socketConfiguration = (io) => {
     io.on('connection', (socket) => {
         console.log('Usuario conectado:', socket.id);
 
-        socket.on('view_denuncia', ({ denunciaId, userId }) => {
-            socket.broadcast.emit('denuncia_en_vista', { denunciaId, userId });
+        socket.on('view_denuncia', async ({ denunciaId, userId }) => {
+            try {
+                await Denuncia.update({ trabajando: userId }, {
+                    where: { idDenuncia: denunciaId }
+                })
+                socket.broadcast.emit('denuncia_en_vista', { denunciaId, userId });
+            } catch (error) {
+                console.log("Error actualizando el usuario trabajando: " , error)
+            }
         });
 
-        socket.on('leave_denuncia', ({ denunciaId }) => {
-            socket.broadcast.emit('denuncia_en_vista', {
-                denunciaId,
-            });
+        socket.on('leave_denuncia', async ({ denunciaId }) => {
+            console.log("Denuncia leave: " , denunciaId)
+            try {
+                await Denuncia.update({ trabajando: null }, {
+                    where: { idDenuncia: denunciaId }
+                })
+                socket.broadcast.emit('denuncia_en_vista', { denunciaId, userId: null });
+            } catch (error) {
+                console.log("Error actualizando el usuario trabajando: " , error)
+            }
         });
 
         socket.on('disconnect', () => {
