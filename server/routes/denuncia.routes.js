6853f3304ea5b4a55388571ec220b2ca0af5218b
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2024-11-26 11:38:34 -0300

    fixes

--- server/routes/denuncia.routes.js
+++ server/routes/denuncia.routes.js
@@ -2,8 +2,9 @@ import express from 'express';
 import { getAllDenuncias, getDenunciaById, createDenuncia, updateDenuncia, deleteDenuncia, countDenunciasSC, getDuplicadas, getAllLike, getAllRegional, denunciaTrabajando } from '../controllers/denuncia.controller.js';
 
 const router = express.Router();
+router.get('/denuncia/count', countDenunciasSC);
 router.get('/denuncia/:clasificada', getAllDenuncias);
-router.get('/denuncia/:id', getDenunciaById);
+router.get('/:id', getDenunciaById);
 router.post('/duplicadas', getDuplicadas)
 router.post('/denuncia', createDenuncia);
 router.post('/trabajando', denunciaTrabajando);
@@ -11,6 +12,5 @@ router.post('/denuncialike', getAllLike);
 router.post('/regional', getAllRegional);
 router.put('/denuncia/:id', updateDenuncia);
 router.delete('/denuncia/:id', deleteDenuncia);
-router.get('/count', countDenunciasSC)
 
 export default router;
