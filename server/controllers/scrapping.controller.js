682b0be44694f1440d77c28d02aa4c6a30b03120
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2024-11-27 11:23:00 -0300

    cambios scrap

--- server/controllers/scrapping.controller.js
+++ server/controllers/scrapping.controller.js
@@ -9,18 +9,21 @@ const getScrapping = async (req, res) => {
 
     let agent;
 
-    if(process.env.NODE_ENV === 'production'){
+    if (process.env.NODE_ENV === 'production') {
         agent = new https.Agent({
-            ca: fs.readFileSync('/etc/letsencrypt/live/srv555183.hstgr.cloud/fullchain.pem'), 
+            ca: [
+                fs.readFileSync('/etc/letsencrypt/live/srv555183.hstgr.cloud/fullchain.pem'), 
+                fs.readFileSync('/etc/letsencrypt/live/srv555183.hstgr.cloud/privkey.pem')
+            ],
             rejectUnauthorized: true
         });
-    }else{
+    } else {
         agent = new https.Agent({
             rejectUnauthorized: false
         });
     }
 
-    
+
     try {
         const { data } = await axios.get(datosMPF.url, { httpsAgent: agent, headers: { Cookie: `PHPSESSID=${datosMPF.cookie}` } });
         const $ = cheerio.load(data)
@@ -31,7 +34,7 @@ const getScrapping = async (req, res) => {
 
         items.push(texto);
 
-        console.log("Informacion: " , items)
+        console.log("Informacion: ", items)
 
         res.status(200).json(items)
     } catch (error) {
